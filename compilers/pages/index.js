fc1e965bab154b80b2f48b0a81dd2a943735b314
Vindexus <vindexus@gmail.com>
2017-01-02 01:59:12 -0800

    I don't know

--- compilers/pages/index.js
+++ compilers/pages/index.js
@@ -1,3 +1,13 @@
-var html = require('./html')
-var indesign = require('./indesign')
-var markdown = require('./markdown')
+var minimist = require('minimist')
+var args = minimist(process.argv.slice(2))
+
+var compilers = ['html', 'indesign', 'markdown', 'website']
+if(args.compilers) {
+  compilers = args.compilers.split(",")
+}
+
+console.log('Running compilers: ' + compilers.join(", "))
+
+compilers.forEach(function (c) {
+  require('./' + c)
+})
