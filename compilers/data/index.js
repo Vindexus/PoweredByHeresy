1c7dc12fc839c07f6e254a3a6de611750b409dc4
Vindexus <vindexus@gmail.com>
2017-01-04 22:24:40 -0800

    Moved game data to website folder

--- compilers/data/index.js
+++ compilers/data/index.js
@@ -6,7 +6,8 @@ var gameDataDir = path.join(__dirname, "..", "..", "source", "data")
 var config = {
   shortcuts: ['moves', 'specialties'], //Data in shortcuts is duplicated into game_data root
   gameDataDir: gameDataDir,
-  outputFile: path.join(__dirname, "..", "..", "game", "game_data.json"),
+  outputFiles: [path.join(__dirname, "..", "..", "game", "game_data.json"),
+    path.join(__dirname, "..", "..", "website", "game_data.json")],
   debug: true,
   pointers: {}
 }
